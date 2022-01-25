60c612a4187a178f39eae4c0e2ad1e00a00fcd4e
Patrick DeVivo <patrick.devivo@gmail.com>
2022-01-25 15:44:01 -0500

    fix: use `math.Ceil(...)`  in `DefaultTimeDiffRangeFormatters` for more consistent phrasing

--- timediff.go
+++ timediff.go
@@ -2,6 +2,7 @@ package timediff
 
 import (
 	"fmt"
+	"math"
 	"sort"
 	"time"
 )
@@ -31,29 +32,29 @@ func WithStartTime(t time.Time) TimeDiffOption {
 
 // DefaultTimeDiffRangeFormatters are the time ranges and their corresponding string formatters.
 var DefaultTimeDiffRangeFormatters = RangeFormatters{
-	-1 << 63:                    func(d time.Duration) string { return fmt.Sprintf("in %.0f years", -d.Hours()/(24.0*30*12)) },
+	-1 << 63:                    func(d time.Duration) string { return fmt.Sprintf("in %.0f years", math.Ceil(-d.Hours()/(24.0*30*12))) },
 	-17 * (24 * time.Hour) * 30: func(_ time.Duration) string { return "in a year" },
-	-10 * (24 * time.Hour) * 30: func(d time.Duration) string { return fmt.Sprintf("in %.0f months", -d.Hours()/(24.0*30)) },
+	-10 * (24 * time.Hour) * 30: func(d time.Duration) string { return fmt.Sprintf("in %.0f months", math.Ceil(-d.Hours()/(24.0*30))) },
 	-45 * (24 * time.Hour):      func(d time.Duration) string { return "in a month" },
-	-25 * (24 * time.Hour):      func(d time.Duration) string { return fmt.Sprintf("in %.0f days", -d.Hours()/24.0) },
+	-25 * (24 * time.Hour):      func(d time.Duration) string { return fmt.Sprintf("in %.0f days", math.Ceil(-d.Hours()/24.0)) },
 	-35 * time.Hour:             func(_ time.Duration) string { return "in a day" },
-	-21 * time.Hour:             func(d time.Duration) string { return fmt.Sprintf("in %.0f hours", -d.Hours()) },
+	-21 * time.Hour:             func(d time.Duration) string { return fmt.Sprintf("in %.0f hours", math.Ceil(-d.Hours())) },
 	-89 * time.Minute:           func(_ time.Duration) string { return "in an hour" },
-	-44 * time.Minute:           func(d time.Duration) string { return fmt.Sprintf("in %.0f minutes", -d.Minutes()) },
+	-44 * time.Minute:           func(d time.Duration) string { return fmt.Sprintf("in %.0f minutes", math.Ceil(-d.Minutes())) },
 	-89 * time.Second:           func(_ time.Duration) string { return "in a minute" },
 	-44 * time.Second:           func(_ time.Duration) string { return "in a few seconds" },
 
 	44 * time.Second:           func(_ time.Duration) string { return "a few seconds ago" },
 	89 * time.Second:           func(_ time.Duration) string { return "a minute ago" },
-	44 * time.Minute:           func(d time.Duration) string { return fmt.Sprintf("%.0f minutes ago", d.Minutes()) },
+	44 * time.Minute:           func(d time.Duration) string { return fmt.Sprintf("%.0f minutes ago", math.Ceil(d.Minutes())) },
 	89 * time.Minute:           func(_ time.Duration) string { return "an hour ago" },
-	21 * time.Hour:             func(d time.Duration) string { return fmt.Sprintf("%.0f hours ago", d.Hours()) },
+	21 * time.Hour:             func(d time.Duration) string { return fmt.Sprintf("%.0f hours ago", math.Ceil(d.Hours())) },
 	35 * time.Hour:             func(_ time.Duration) string { return "a day ago" },
-	25 * (24 * time.Hour):      func(d time.Duration) string { return fmt.Sprintf("%.0f days ago", d.Hours()/24.0) },
+	25 * (24 * time.Hour):      func(d time.Duration) string { return fmt.Sprintf("%.0f days ago", math.Ceil(d.Hours()/24.0)) },
 	45 * (24 * time.Hour):      func(d time.Duration) string { return "a month ago" },
-	10 * (24 * time.Hour) * 30: func(d time.Duration) string { return fmt.Sprintf("%.0f months ago", d.Hours()/(24.0*30)) },
+	10 * (24 * time.Hour) * 30: func(d time.Duration) string { return fmt.Sprintf("%.0f months ago", math.Ceil(d.Hours()/(24.0*30))) },
 	17 * (24 * time.Hour) * 30: func(_ time.Duration) string { return "a year ago" },
-	1<<63 - 1:                  func(d time.Duration) string { return fmt.Sprintf("%.0f years ago", d.Hours()/(24.0*30*12)) },
+	1<<63 - 1:                  func(d time.Duration) string { return fmt.Sprintf("%.0f years ago", math.Ceil(d.Hours()/(24.0*30*12))) },
 }
 
 // TimeDiff prints a human-readable string representing the difference between two `time.Time`s.
