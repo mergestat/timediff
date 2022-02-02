a48a8dcd1bee6f7e31cb46e1d80846fbb13ddeda
Patrick DeVivo <patrick.devivo@gmail.com>
2022-02-01 20:38:00 -0500

    feat: allow users to supply custom formatters that override those from the locale

--- timediff.go
+++ timediff.go
@@ -1,9 +1,10 @@
 package timediff
 
 import (
-	"github.com/mergestat/timediff/locale"
 	"sort"
 	"time"
+
+	"github.com/mergestat/timediff/locale"
 )
 
 // TimeDiffOption is an option used to customize a call to TimeDiff
@@ -15,6 +16,9 @@ type timeDiffOptions struct {
 
 	// Locale is the locale string used by TimeDiff function.
 	Locale locale.Locale
+
+	// CustomFormatters allows the caller to override the formatters used with their own
+	CustomFormatters locale.Formatters
 }
 
 // WithStartTime changes the start time from which time diff calculations are made.
@@ -32,6 +36,13 @@ func WithLocale(locale locale.Locale) TimeDiffOption {
 	}
 }
 
+// WithCustomFormatters overrides the formatters supplied by any registered and used locale
+func WithCustomFormatters(f locale.Formatters) TimeDiffOption {
+	return func(opt *timeDiffOptions) {
+		opt.CustomFormatters = f
+	}
+}
+
 // TimeDiff prints a human-readable string representing the difference between two `time.Time`s.
 // By default, the "start" time is time.Now(), but can be overridden with the WithStartTime(t time.Time) option.
 func TimeDiff(t time.Time, options ...TimeDiffOption) string {
@@ -44,8 +55,12 @@ func TimeDiff(t time.Time, options ...TimeDiffOption) string {
 	}
 
 	var rf locale.Formatters
-	if rf = locale.Lookup(opt.Locale); rf == nil {
-		return "" // TODO: find out a way to return an error in case locale is not found
+	if opt.CustomFormatters == nil {
+		if rf = locale.Lookup(opt.Locale); rf == nil {
+			return "" // TODO: find out a way to return an error in case locale is not found
+		}
+	} else {
+		rf = opt.CustomFormatters
 	}
 
 	// break up the range into slices of keys (durations) and values (formatters)
